bb1eb8ea552dbbce6ce28b8c3cd9cd364e1f87e7
Mahin Katariya <mahinkatariya31@gmail.com>
2026-05-14 21:54:03 +0530

    react project setup

---
+++ foundations-07/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Use the Tailwind Prettier plugin to sort classnames automatically
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'avoid',
+};
